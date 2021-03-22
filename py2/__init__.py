2bf877c9f504253b86c47db972273b6bc6a638d6
S0nic014 <47799401+S0nic014@users.noreply.github.com>
2021-03-22 16:24:28 +0000

    py2 init imports

---
+++ py2/__init__.py
@@ -0,0 +1,7 @@
+from qt_widgets_lib.py2.field_widgets import NumericFieldWidget
+from qt_widgets_lib.py2.field_widgets import StringFieldWidget
+from qt_widgets_lib.py2.list_widgets import ItemsListGroup
+from qt_widgets_lib.py2.path_widget import PathWidget
+from qt_widgets_lib.py2.scrollable_widget import ScrollWidget
+from qt_widgets_lib.py2.section_widget import SectionWidget
+from qt_widgets_lib.py2.time_range import TimeRangeWidget
